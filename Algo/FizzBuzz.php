b9ccd64a7feeca709a4af7a58ec7138d2fafe737
Joris <joris.courtois.ext@sogetrel.fr>
2022-08-14 11:20:32 +0200

    ADD Algo test

---
+++ Algo/FizzBuzz.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Algo\FizzBuzz;
+
+class FizzBuzz
+{
+    /**
+     * @param int $number
+     * @return int|string
+     * @throws \Exception
+     */
+    public function display(int $number): void
+    {
+        if (1 > $number) {
+           throw new \Exception('Number should be greater or equal than 1');
+        }
+
+        if (0 !== $number % 3 && 0 != $number % 5) {
+            echo $number;
+
+            return;
+        }
+
+        $result = '';
+
+        if (0 === $number % 3) {
+            $result .= 'Fizz';
+        }
+
+        if (0 === $number % 5) {
+            $result .= 'Buzz';
+        }
+
+        echo $result;
+    }
+}
